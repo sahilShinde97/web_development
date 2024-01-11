bb58579cd098c7807217012b8b0be32de4ba1f31
Sahil Shinde <sahilshinde9730@gmail.com>
2024-01-11 22:37:39 +0530

    Advanced Function

--- To Do List/ToDo.js
+++ To Do List/ToDo.js
@@ -5,7 +5,7 @@ let todoList = [
    },
 
    {
-    item:'go to collage',
+    item:'Go to collage',
     dueDate:'4/10/2024',
    }
   
@@ -15,14 +15,13 @@ let todoList = [
 displayItems();
 
 function addTodo() {
-  
   let inputElement = document.querySelector
   ('#todo-input');
   let dateElement = document.querySelector
   ('#todo-date');
   let todoItem = inputElement.value;
   let todoDate = dateElement.value;
-  todoList.push({item: todoItem ,dueDate: todoDate});
+  todoList.push({ item: todoItem, dueDate: todoDate }); 
   inputElement.value = '';
   dateElement.value = '';
 
@@ -34,16 +33,14 @@ function displayItems()
   let containerElement = document.querySelector('.todo-container');
   let newHtml = '';
    for(let i = 0; i < todoList.length; i++){
-    let todoItem = todoList[i].item;
-    let dueDate = todoList[i].dueDate;
+    let {item,dueDate}=todoList[i];
     newHtml += `
-    <div>
-    span>${item}</span>
-    span>${dueDate}</span>
-
-    <button onclick="todoList.splice(${i}, 1); 
-    displayItems();">Delete</button>
-    </div>
+      
+        <span>${item}</span>
+        <span>${dueDate}</span>
+        <button class="btn-delete" onclick="todoList.splice(${i}, 1); 
+        displayItems();">Delete</button>
+    
     `;
    }
    containerElement.innerHTML = newHtml;
